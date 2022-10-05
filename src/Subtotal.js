dddf1aec8d1983c40d40d5b609c372f07e0706db
jozebel11 <jjhardwicke@hotmail.co.uk>
2022-10-05 11:51:27 +0100

    Update Checkout.js, Subtotal.js, reducer.js to add basket products to a total.  Add two new files to show added product in checkout page

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -13,8 +13,8 @@ function Subtotal() {
            renderText={(value) => (
             <>
               <p>
-                Subtotal ({basket.length}, items):
-                <strong>{` ${value}`}</strong>
+                Subtotal ({basket.length} items):
+                <strong>{value}</strong>
               </p>
               <small className='subtotal_gift'>
                 <input type='checkbox' /> This order contains a gift
